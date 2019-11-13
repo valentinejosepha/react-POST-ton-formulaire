887e26ad227c6460753f47bf566c6bbbeb10ef91
Valentine Voyen <valentine.voyen1@gmail.com>
2019-11-13 16:48:39 +0100

    firsts fixes

--- src/components/FormCinema.jsx
+++ src/components/FormCinema.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class FormCinema extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       title: '',
@@ -9,20 +9,23 @@ class FormCinema extends Component {
       comment: ''
     }
     this.onChange = this.onChange.bind(this)
-    this.submitForm = this.submitForm.bind(this)
+    this.submitForm = this.postData.bind(this)
   }
 
   onChange(e) {
     this.setState({
-      [e.target.title] : e.target.value,
+      [e.target.title]: e.target.value,
     });
   }
 
   submitForm(e) {
     e.preventDefault();
+    this.postData();
   }
 
-  render () {
+
+
+  postData() {
     const config = {
       method: "POST",
       headers: {
@@ -30,11 +33,11 @@ class FormCinema extends Component {
       },
       body: JSON.stringify(this.state),
     };
-  
+
     const url = "https://post-a-form.herokuapp.com/api/movies/";
 
     fetch(url, config)
-    .then(res => res.json())
+      .then(res => res.json())
       .then(res => {
         if (res.error) {
           alert(res.error);
@@ -45,53 +48,55 @@ class FormCinema extends Component {
         console.error(e);
         alert(`Erreur lors de l'ajout d'un film`)
       });
+  }
 
-    return(
-      <div className="FormEmployee">
-      <h1>Cherche un film!</h1>
+  render() {
+    return (
+      <div className="FormCinema">
+        <h1>Cherche un film!</h1>
 
-      <form onSubmit={this.submitForm}>
-        <fieldset>
-          <legend>Informations</legend>
-          <div className="form-data">
-            <label htmlFor="title">Titre</label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              onChange={this.onChange}
-              value={this.state.title}
-            />
-          </div>
+        <form onSubmit={this.submitForm}>
+          <fieldset>
+            <legend>Informations</legend>
+            <div className="form-data">
+              <label htmlFor="title">Titre</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                onChange={this.onChange}
+                //value={this.state.title}
+              />
+            </div>
 
-          <div className="form-data">
-            <label htmlFor="poster">Prénom</label>
-            <input 
-              type="url"
-              id="poster"
-              name="poster"
-              onChange={this.onChange}
-              value={this.state.poster}
-            />
-          </div>
+            <div className="form-data">
+              <label htmlFor="poster">Lien du poster</label>
+              <input
+                type="url"
+                id="poster"
+                name="poster"
+                onChange={this.onChange}
+                //value={this.state.poster}
+              />
+            </div>
 
-          <div className="form-data">
-            <label htmlFor="comment">E-mail</label>
-            <textarea 
-              type="text"
-              id="comment"
-              name="comment"
-              onChange={this.onChange}
-              value={this.state.comment}
-            />
-          </div>
-          <hr />
-          <div className="form-data">
-            <input type="submit" value="Envoyer" />
-          </div>
-        </fieldset>
-      </form>
-    </div>
+            <div className="form-data">
+              <label htmlFor="comment">Commentaire</label>
+              <textarea
+                type="text"
+                id="comment"
+                name="comment"
+                onChange={this.onChange}
+                //value={this.state.comment}
+              />
+            </div>
+            <hr />
+            <div className="form-data">
+              <input type="submit" value="Envoyer" />
+            </div>
+          </fieldset>
+        </form>
+      </div>
     );
   }
 }
